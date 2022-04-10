cd79e0d83a2a2bc0c0dc3ac518163374aa2f3dab
Daniel Svens <danielsvens@hotmail.com>
2022-04-10 14:40:05 +0200

    Changed url

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://step-counter-api:8083/graphql',
+  uri: 'http://http://192.168.1.4:32022/graphql',
   cache: new InMemoryCache(),
 });
 
